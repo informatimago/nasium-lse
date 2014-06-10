f59b0da24002b7ec1998017be0e2c5b28a01de1e
Pascal J. Bourguignon <pjb@informatimago.com>
2014-06-10 09:49:23 +0200

    Updated inetd and siab scripts.

--- siab/lse-siab.sh
+++ siab/lse-siab.sh
@@ -3,10 +3,10 @@ export LC_ALL=en_US.UTF-8
 export TERM=vt100
 export LSE_TELNET=NIL
 export LSE_PAGER=NIL
-export LSE_ROOT=/home/lse/files/
+export LSE_ROOT=/srv/lse/files/
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib
 cd $LSE_ROOT
-exec /home/lse/bin/lse \
+exec /srv/lse/bin/lse \
     --mode-ancien \
     --sans-bip \
     --fleches-unicode \

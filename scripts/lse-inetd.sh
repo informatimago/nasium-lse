2d8ca7fb0a1789f220c77e7bdb5ec3eb53db35a4
Pascal J. Bourguignon <pjb@informatimago.com>
2014-06-27 02:59:55 +0200

    Moved scripts to scripts/

--- scripts/lse-inetd.sh
+++ scripts/lse-inetd.sh
@@ -6,9 +6,11 @@ export LSE_ROOT=/srv/lse/files/
 cd $LSE_ROOT
 exec /srv/lse/bin/lse \
     --mode-moderne \
+    --sans-bip \
     --fleches-ascii \
     --entree-comme-xoff \
     --afficher-sans-accent \
-    --afficher-en-majuscules \
     --accepter-minuscules \
-    --montrer-touches 
+    --affichage-mixte
+#--- 
+    --afficher-en-majuscules \

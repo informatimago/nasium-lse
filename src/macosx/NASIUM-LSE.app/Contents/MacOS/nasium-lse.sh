3aaae68e310435033c19cb26aac45542e1f93795
Pascal J. Bourguignon <pjb@informatimago.com>
2016-02-06 17:40:51 +0100

    Bumped version number.

--- src/macosx/NASIUM-LSE.app/Contents/MacOS/nasium-lse.sh
+++ src/macosx/NASIUM-LSE.app/Contents/MacOS/nasium-lse.sh
@@ -2,10 +2,11 @@
 cd "$(dirname "$0")/../.."
 APP="$(pwd)"
 Terminal_PROFILE="$APP/Contents/Resources/nasium-lse.terminal"
-NasiumLSE_PGM="$APP/Contents/MacOS/lse-ccl-darwin-apple-11.3.0-i386"
+NasiumLSE_PGM="$APP/Contents/MacOS/lse-ccl-darwin-apple-14.1.0-x86-64"
 exec osascript <<EOF
 tell app "Terminal" 
     open "${Terminal_PROFILE}"
-    do script "${NasiumLSE_PGM} ; exit" in first window
+    do script "${NasiumLSE_PGM} --modern-mode --accept-lowcase --mixed-output --accented-output --bell --unicode-halfwidth-arrows --show-bindings ; exit" in first window
 end tell
 EOF
+# --configuration-macosx-terminal
